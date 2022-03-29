697943e549b6316553ddc3ee886999db4b0ab689
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2022-03-29 11:54:39 +0200

    feat: app alien game

---
+++ src/components/GamestateFinish.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal">
+    <div v-if="uiState === 'won'">
+        <h2>You won</h2>
+    </div>
+    <div v-else>
+        <h2>You lost</h2>
+    </div>
+    <p>
+      <button @click="restart">Let's play again!</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  import { mapState } from "vuex"
+
+  export default {
+    computed: {
+      ...mapState("uiState")
+    },
+    methods: {
+      restart() {
+        this.$store.commit("restartGame")
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
